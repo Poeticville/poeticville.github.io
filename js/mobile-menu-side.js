797188f60cc27a611b74cbc0f556c6bd91f35ce0
Neutron <neutrolysis@gmail.com>
2019-03-07 01:49:13 +0100

    initial commit

--- js/mobile-menu-side.js
+++ js/mobile-menu-side.js
@@ -1,6 +1,6 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("mySidebar").style.width = "300px";
+  document.getElementById("mySidebar").style.width = "280px";
   document.getElementById("main").style.marginLeft = "250px";
 }
 
